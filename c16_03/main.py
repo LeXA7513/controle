c9589de661f8d7ff43eed49c803ea203bca8b369
LeXA7513 <autogue.axel@gmail.com>
2021-03-16 10:51:31 +0000

    function 4c

--- c16_03/main.py
+++ c16_03/main.py
@@ -10,4 +10,30 @@ note8 = ('eleve2', 'math', 14)
 notes = [note1, note2, note3, note4, note5, note6,note7,note8]
 
 print(note1)
-mean(notes[[3]])
+
+def moyenne_tuples(liste,nom="tous",matiere="toutes"):
+  res=0
+  n=0
+  for a in liste :
+    if (nom == "tous"):
+      if (matiere == "toutes") :
+          res=res+a[2]
+          n=n+1
+      else :
+        if (a[1]== str(matiere)):
+         res=res+a[2]
+         n=n+1      
+    else :
+      if (a[0]== str(nom)) :
+          if (matiere == "toutes") :
+              res=res+a[2]
+              n=n+1
+          else :
+            if (a[1]== str(matiere)):
+              res=res+a[2]
+              n=n+1
+  res=round(res/n,2)
+  return print(res)
+
+moyenne_tuples(notes,"eleve1")
+moyenne_tuples(notes,"eleve1","math")
