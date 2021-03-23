c411707023c5b2eab801d0a7abfeb5330cf208fc
LeXA7513 <autogue.axel@gmail.com>
2021-03-23 17:34:48 +0000

    Présentation  des réponses + Question 8

--- c16_03/main.py
+++ c16_03/main.py
@@ -21,14 +21,15 @@ def moyenne_tuples(liste,nom=None,matiere=None):
     moy = sum(res)/len(res)
   return moy
 
-# Question 4 a
+print("Question 4 a")
 print(moyenne_tuples(notes,"eleve1",))
 
-# Question 4 b
+print("Question 4 b")
 print(moyenne_tuples(notes,"eleve1","math"))
 
 
-# Question 5 
+#Question 5
+
 notes_enregistrées = []
 class Note:
   def __init__(self, eleve, matiere, valeur):
@@ -44,45 +45,38 @@ class Note:
   def __str__(self):
     return f"Eleve: {self.eleve} Matiére : {self.matiere} Note : {self.valeur}"
 
-onote = Note('eleve1', 'maths', 13)
-print(onote.eleve)
-print(onote.matiere)
-print(onote.valeur)
-Note.afficher(onote)
+
 onotes = []
-
-
 for a in notes:
   onotes.append(Note(a[0],a[1],a[2]))
 #Pour afficher l'ensemble des notes avec la fonction Note.afficher()
 #for x in range(len(onotes)) :
 #  Note.afficher(onotes[x])
 
-# Question 6
+print("Question 6")
 #Afficher l'ensemble des notes avec la fonction prin()
 for x in range(len(onotes)) :
   print(onotes[x])
 
-# Question 7
+print("Question 7")
+#Démonstration de la sauvegarde des notes de facon automatique
 for x in range(len(notes_enregistrées)) :
   print(notes_enregistrées[x])
 
+print("Question 8")
 
+def moyenne_Notes(nom=None,matiere=None):
+  liste = notes_enregistrées
+  res = []
+  liste_eleve=[]
+  for a in liste :
+    liste_eleve= [x for x in liste if x.eleve == nom or nom == None]
+    liste_matiere= [x for x in liste_eleve if x.matiere == matiere or matiere == None]
+    res = [x.valeur for x in liste_matiere ]
+    moy = sum(res)/len(res)
+  return moy
 
-
-
-# Question 8
-#def moyenne_Notes(liste,nom = None,matiere = None):
-#  res = []
-#  liste_eleve=[]
-#  for a in liste :
-#    liste_eleve= [x for x in liste if x.eleve == nom or nom == None]
-#    liste_matiere= [x for x in liste_eleve if x.matiere == matiere or matiere == None]
-#    res = [x.valeur for x in liste_matiere ]
-#    moy = sum(res)/len(res)
-#  return moy
-
-#print(moyenne_Notes(notes_enregistrées))
+print(moyenne_Notes())
 
 #Question 9
 
